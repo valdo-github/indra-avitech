2023057d795811760da70368e4bd4a31c579284f
michal-velecy <michal.velecky@gmail.com>
2025-03-29 05:07:40 +0100

    1

--- src/main/java/indra/avitechcom/repository/UserRepository.java
+++ src/main/java/indra/avitechcom/repository/UserRepository.java
@@ -2,7 +2,6 @@ package indra.avitechcom.repository;
 
 import indra.avitechcom.entity.User;
 import indra.avitechcom.hibernate.HibernateUtil;
-import indra.avitechcom.service.CommandService;
 import lombok.Getter;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
--- src/test/java/indra/avitechcom/service/CommandServiceTest.java
+++ src/test/java/indra/avitechcom/service/CommandServiceTest.java
@@ -3,46 +3,37 @@ package indra.avitechcom.service;
 import indra.avitechcom.entity.User;
 import indra.avitechcom.model.UserDTO;
 import indra.avitechcom.repository.UserRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @ExtendWith(MockitoExtension.class)
 public class CommandServiceTest {
 
+    private static final PodamFactory factory = new PodamFactoryImpl();
+
     private static UserRepository repository = Mockito.mock(UserRepository.class);
 
-//    @Mock
-//    private UserRepository repository;
-//
-//    @BeforeEach
-//    void before() {
-//        UserRepository.setINSTANCE(repository);
-//    }
-
-    private static final PodamFactory factory = new PodamFactoryImpl();
+    @BeforeEach
+    void before() {
+        UserRepository.setINSTANCE(repository);
+    }
 
     @Captor
     private ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
 
     @Test
     public void testSave() {
-//        UserRepository repository = Mockito.mock(UserRepository.class);
-        UserRepository.setINSTANCE(repository);
-
 
         UserDTO user = factory.manufacturePojo(UserDTO.class);
 
@@ -60,8 +51,6 @@ public class CommandServiceTest {
 
     @Test
     public void testPrintAll() {
-//        UserRepository repository = Mockito.mock(UserRepository.class);
-        UserRepository.setINSTANCE(repository);
 
         CommandService.getInstance().printAll();
 
@@ -71,8 +60,6 @@ public class CommandServiceTest {
 
     @Test
     public void testDeleteAll() {
-//        UserRepository repository = Mockito.mock(UserRepository.class);
-        UserRepository.setINSTANCE(repository);
 
         CommandService.getInstance().deleteAll();
 
